e3b2b895305357bee5786bfaa4836dd66bb31438
Caracalla1081 <Caracalla1081@gmail.com>
2023-05-07 14:26:15 -0400

    Code rework

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,11 +2,12 @@
 console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Add marker for Los Angeles
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
 // Add marker for Los Angeles to map
 L.circle([34.0522, -118.2437], {
     radius: 100
  }).addTo(map);
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
