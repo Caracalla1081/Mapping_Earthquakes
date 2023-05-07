70f5e294b6602e419aba78c59932c2aa16959eb2
Caracalla1081 <Caracalla1081@gmail.com>
2023-05-07 14:47:45 -0400

    Added popup

--- Mapping_Multiple_Points/static/js/logic2.js
+++ Mapping_Multiple_Points/static/js/logic2.js
@@ -6,9 +6,12 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
+// Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
     console.log(city)
-    L.marker(city.location).addTo(map);
+    L.marker(city.location)
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  .addTo(map);
 });
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
