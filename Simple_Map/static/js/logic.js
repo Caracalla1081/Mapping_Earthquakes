fc44d1f93009a5d0a927b890bcfb5a81788fd4b1
Caracalla1081 <Caracalla1081@gmail.com>
2023-05-07 14:25:09 -0400

    Updated marker to circle

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,6 +2,10 @@
 console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Add marker for Los Angeles to map
+L.circle([34.0522, -118.2437], {
+    radius: 100
+ }).addTo(map);
 let marker = L.marker([34.0522, -118.2437]).addTo(map);
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
